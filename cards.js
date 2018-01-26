13d71a0b994196072230d6c8efe4518e3e77a27e
NinaW <ninakwelch@gmail.com>
2018-01-26 10:17:43 -0800

    card 20 edit

--- cards.js
+++ cards.js
@@ -9,7 +9,7 @@
 // This function will select all ids that start with 'card-''
 */
 
-$( "[id^='card-']" ).css( "border", "3px dotted green" );
+//$( "[id^='card-']" ).css( "border", "3px dotted green" );
 
 /*
 // :contains() Selector
@@ -18,7 +18,7 @@ $( "[id^='card-']" ).css( "border", "3px dotted green" );
 */
 
 
-$( "div:contains('dog')" ).css( "text-decoration", "underline" );
+//$( "div:contains('dog')" ).css( "text-decoration", "underline" );
 
 
 /*
@@ -26,7 +26,7 @@ $( "div:contains('dog')" ).css( "text-decoration", "underline" );
 //
 //
 */
-$( "[id^='card-']" ).attr('class', 'newClass');
+//$( "[id^='card-']" ).attr('class', 'newClass');
 
 /*
 //
@@ -34,14 +34,23 @@ $( "[id^='card-']" ).attr('class', 'newClass');
 //
 */
 
+// processClicks(1);
 
+function processClicks(cardIndex) {
+//  var cardIndex = 1;
+  $( "[id^='card-']" ).attr('class', 'style'+cardIndex);
+  $( "[class^='card-title']" ).attr('class', 'card-title'+cardIndex);
+//  $( "[class^='card-img']" ).attr('class', 'card-img'+1);
+//  $( "[class^='card-body']" ).attr('class', 'card-body'+cardIndex);
+}
 
-
-
-
-
-
-
-
+function reset() {
+	for (var i = 1; i <= 2; i++) {
+		var temp = 'card-00'+i; // TODO: take off the 00
+		$( "[id="+temp+"]" ).attr('class', 'style'+i);
+		$( "[id="+temp+"]" ).find( "[class^='card-title']" ).attr( "class", "card-title"+i );
+//		$( "[id="+temp+"][class^="card-title"]" ).attr('class', 'card-title'+i);
+	}
+}
 
 
