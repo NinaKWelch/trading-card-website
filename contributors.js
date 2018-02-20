90bf607c7fbb8e9a9dbca1d474ba49c059b759c1
NinaW <ninakwelch@gmail.com>
2018-02-20 23:20:27 +0000

    Changes to contributors

--- contributors.js
+++ contributors.js
@@ -1,7 +1,7 @@
 /* Javascript for Contibutors Page */
 
 
-// Show contributors in selected country
+// Show contributors in a selected country when clicking on the country flag
 function selectCountry(countryIndex) {
 	var country = $('[id^=in-]');
 	country.hide();
@@ -44,4 +44,50 @@ function selectCountry(countryIndex) {
     	country.show();
     }
 }
+
+
+// Show contributors in a selected country, when chosen in the options menu
+$('#inputGroupCountries').on('change', function() {
+	var country = $('[id^=in-]');
+	country.hide();
+
+	if (this.value == "0") {
+		country.show();
+	} else if (this.value == "1") {
+	    country.filter('#in-bg').toggle();
+	} else if (this.value == "2") {
+        country.filter('#in-hr').toggle();
+	} else if (this.value == "3") {
+       country.filter('#in-eg').toggle();
+	} else if (this.value == "4") {
+		country.filter('#in-fi').toggle();
+	} else if (this.value == "5") {
+		country.filter('#in-de').toggle();
+	} else if (this.value == "6") {
+		country.filter('#in-gr').toggle();
+	} else if (this.value == "7") {
+		country.filter('#in-hu').toggle();
+	} else if (this.value == "8") {
+		country.filter('#in-il').toggle();
+	} else if (this.value == "9") {
+		country.filter('#in-it').toggle();
+	} else if (this.value == "10") {
+		country.filter('#in-pl').toggle();
+	} else if (this.value == "11") {
+		country.filter('#in-pt').toggle();
+	} else if (this.value == "12") {
+		country.filter('#in-ro').toggle();
+	} else if (this.value == "13") {
+		country.filter('#in-sk').toggle();
+	} else if (this.value == "14") {
+		country.filter('#in-es').toggle();
+	} else if (this.value == "15") {
+		country.filter('#in-tr').toggle();
+	} else if (this.value == "16") {
+		country.filter('#in-gb').toggle();
+	} else {
+		country.show();
+	}
+});
+
 
