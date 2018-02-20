2bedad1b229c64fb70f80dabbc8dd3172d2b4999
NinaW <35150414+NinaKWelch@users.noreply.github.com>
2018-02-20 16:38:42 +0000

    tidy up

--- cards.js
+++ cards.js
@@ -89,21 +89,3 @@ $(document).ready(function() {
         bg.addClass('style59-bg');
     });
 });
-
-
-/*
-// Attribute Starts With Selector [name^=”value”]
-// Selects elements that have the specified attribute with a value beginning exactly with a given string.
-// This function will select all ids that start with 'card-''
-*/
-
-//$( "[id^='card-']" ).css( "border", "3px dotted green" );
-
-/*
-// :contains() Selector
-// Select all elements that contain the specified text.
-// This function selects all card that contain a word 'dog'
-*/
-
-
-//$( "div:contains('dog')" ).css( "text-decoration", "underline" );
